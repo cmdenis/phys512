84a2d080d4f3cef60477431b360be280095c69a9
cmdenis <christianmdenis@gmail.com>
2022-09-19 12:56:55 -0400

    started a2

--- Assignment 2/a2_q1.py
+++ Assignment 2/a2_q1.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sci
+
+def ef_ring(z, Q = 1, R = 1):
+    return Q*z/(4*np.pi*sci.constants.epsilon_0)
+
+print(sci.constants.epsilon_0)
+
