08a9c8deab5443709bd92a4a1338df274aeb4c9a
cmdenis <christianmdenis@gmail.com>
2022-09-19 22:40:08 -0400

    advanced q1, but stuck with code, started q2

--- Assignment 2/a2_q1.py
+++ Assignment 2/a2_q1.py
@@ -1,15 +1,34 @@
+from ast import arg
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sci
 import scipy.constants as cons
+from scipy import integrate
+
+R = 1 # We define the radius of the ball to be 1
+Q = (4 * np.pi * R**2)**-1 # We define the surface charge density
 
 
-def ef_ring(z, Q = 1, R = 1):
+def ef_ring(r, z):
     '''Function for electric field of a ring along central axis.'''
-    return -Q*z/(4*np.pi*cons.epsilon_0*(z**2+R**2)**(3/2))
+    return -Q*z/(4*np.pi*cons.epsilon_0*(z**2+r**2)**(3/2))
+
+# Since we have a ball, let's assume the test charge is at a distance z away from the ball
+# As we change the radius, the rings get closer and smaller from that point that is not moving
+# We must find the relationship between the radius of the ring and its "z" coordinate, it is:
+def new_z(r):
+    return np.sqrt(1-r/R)
+
+def int_rings(theta, z1):
+
+    z = z1 - np.sin(theta) # To account for proximity of rings
+    r = np.cos(theta)
+    return -Q*z/(4*np.pi*cons.epsilon_0*(z**2+r**2)**(3/2))*(2*np.pi*R)*np.sin(theta)*R
+
+
 
 v = np.linspace(-4, 4, 1001)
-y_data = ef_ring(v)
+y_data = ef_ring(1, v)
 
 
 plt.plot(v, y_data)
@@ -17,8 +36,28 @@ plt.title("Electric field along central axis of ring")
 plt.xlabel("Distance from center")
 plt.ylabel("Electric Field")
 plt.savefig("figs/q1_ring_ef.jpg")
-plt.show()
+#plt.show()
 
 
 
+# Integrating all rings from with scipy.quad
+
+def test(x, a):
+    return a*np.exp(x)
+
+
+def quad_shell_ef(z):
+
+    return integrate.quad(ef_ring, 1)
+
+
+def integrand(a, b): 
+    #print(theta, t)
+    return np.exp(a)*0 + b
+
+#print(integrate.quad(test, 0, 1), args = (1,))
+
+print(integrate.quad(integrand, 0, 1, args=(3,)))
+
+print(integrate.quad(int_rings, -R, R, args=(5,)))
 
---
+++ Assignment 2/test.py
@@ -0,0 +1,32 @@
+from ast import Lambda, arg
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sci
+import scipy.constants as cons
+from scipy import integrate
+
+R = 1
+sigma = 0.1*(4 * np.pi * R**2)**-1
+
+def to_int(theta, z):
+    coeff = 2*np.pi*R**2*sigma/(4*np.pi*cons.epsilon_0)
+    return coeff*(z-R*np.sin(theta))/(R**2 + z**2 - 2*R*z*np.cos(theta))**(3/2)
+
+#print(integrate.quad(to_int, -np.pi/2, np.pi/2, args=(5,)))
+
+def efield(z):
+    temp_func = lambda theta : to_int(theta, z) # Anonymous function
+
+    return integrate.quad(temp_func, 0, np.pi)
+
+print(0.1/(4*np.pi*cons.epsilon_0*1))
+
+
+v = np.linspace(1.1, 4, 1001)
+y_data = [efield(i)[0] for i in v]
+y_coul = 0.1/(4*np.pi*cons.epsilon_0*v**2)
+
+plt.clf()
+plt.plot(v, y_data, label = "")
+plt.plot(v, y_coul)
+plt.show()
