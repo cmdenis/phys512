512cb814947b9dc32447a7f80bb4e43593f38297
cmdenis <69637028+cmdenis@users.noreply.github.com>
2022-09-20 17:03:07 -0400

    Update a2_q2.py
    updated q2

--- Assignment 2/a2_q2.py
+++ Assignment 2/a2_q2.py
@@ -25,13 +25,12 @@ def integrate(fun,a,b,tol):
 
 def integrate_adaptive(fun, a, b, tol, extra = None):
     print('calling adaptive function from ', a, b)
-    nb_points = 5
     if extra == None:
-        x = np.linspace(a, b, nb_points)
+        x = np.linspace(a, b, 5)
         dx = x[1] - x[0]
         y = fun(x)
     else:
-        x = np.linspace(a, b, nb_points)
+        x = np.linspace(a, b, 5)
         dx = x[1] - x[0]
         y = np.array([extra[0], fun(x[1]), extra[1], fun(x[3]), extra[2]])
 
@@ -57,3 +56,36 @@ print('Normal Integrator:', ans, ans2, ans-(10+np.sqrt(2*np.pi)))
 ans = integrate_adaptive(offset_gauss, -4, 6, 1e-6)
 ans2 = integrate_adaptive(offset_gauss, -4, 0, 1e-6) + integrate_adaptive(offset_gauss, 0, 6,1e-6)
 print('Adaptive Integrator:', ans, ans2, ans-(10+np.sqrt(2*np.pi)))
+
+import numpy as np
+
+def heaviside(x):
+    return 1.0*(x>0)
+
+def offset_gauss(x):
+    return 1+10*np.exp(-0.5*x**2/(0.1)**2)
+
+
+def integrate(fun,a,b,tol):
+    print('calling function from ',a,b)
+    x=np.linspace(a,b,5)
+    dx=x[1]-x[0]
+    y=fun(x)
+    #do the 3-point integral
+    i1=(y[0]+4*y[2]+y[4])/3*(2*dx)
+    i2=(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3*dx
+    myerr=np.abs(i1-i2)
+    if myerr<tol:
+        return i2
+    else:
+        mid=(a+b)/2
+        int1=integrate(fun,a,mid,tol/2)
+        int2=integrate(fun,mid,b,tol/2)
+        return int1+int2
+
+
+ans=integrate(offset_gauss,-4,6,1e-6)
+ans2=integrate(offset_gauss,-4,0,1e-6)+integrate(offset_gauss,0,6,1e-6)
+print('answer was ',ans,ans2,ans-(10+np.sqrt(2*np.pi)))
+#print('answer was ',ans,ans-(np.exp(1)-np.exp(-1)))
+
