bc47a5b980b6c86148c9791027db44b5ae572a12
cmdenis <christianmdenis@gmail.com>
2022-09-20 22:26:59 -0400

    tried to sync up changes

--- Assignment 2/a2_q2.py
+++ Assignment 2/a2_q2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sci
 
+
 def offset_gauss(x):
     return 1+10*np.exp(-0.5*x**2/(0.1)**2)
 
@@ -22,14 +23,36 @@ def integrate(fun,a,b,tol):
         int2=integrate(fun,mid,b,tol/2)
         return int1+int2
 
+def integrate_adaptive(fun, a, b, tol, extra = None):
+    print('calling adaptive function from ', a, b)
+    if extra == None:
+        x = np.linspace(a, b, 5)
+        dx = x[1] - x[0]
+        y = fun(x)
+    else:
+        x = np.linspace(a, b, 5)
+        dx = x[1] - x[0]
+        y = np.array([extra[0], fun(x[1]), extra[1], fun(x[3]), extra[2]])
+
+    #do the 3-point integral
+    i1 = (y[0]+4*y[2]+y[4])/3*(2*dx)
+    i2 = (y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3*dx
+    myerr = np.abs(i1-i2)
+    if myerr < tol:
+        return i2
+    else:
+        mid = (a+b)/2
+        int1 = integrate_adaptive(fun, a, mid, tol/2, extra = [y[0], y[1], y[2]])
+        int2 = integrate_adaptive(fun, mid, b, tol/2, extra = [y[2], y[3], y[4]])
+        return int1+int2
+
 # To answer the question, we should basically pass the x vector in the extra argument and then we will only need to compute two of the 5 values of the vector
 
-ans=integrate(offset_gauss,-4,6,1e-6)
-ans2=integrate(offset_gauss,-4,0,1e-6)+integrate(offset_gauss,0,6,1e-6)
-print('answer was ',ans,ans2,ans-(10+np.sqrt(2*np.pi)))
-#print('answer was ',ans,ans-(np.exp(1)-np.exp(-1)))
+ans = integrate(offset_gauss, -4, 6, 1e-6)
+ans2 = integrate(offset_gauss, -4, 0, 1e-6) + integrate(offset_gauss, 0, 6,1e-6)
+print('Normal Integrator:', ans, ans2, ans-(10+np.sqrt(2*np.pi)))
 
 
-def integrate_adaptive(fun, a, b, tol, extra = None):
-
-    return
+ans = integrate_adaptive(offset_gauss, -4, 6, 1e-6)
+ans2 = integrate_adaptive(offset_gauss, -4, 0, 1e-6) + integrate_adaptive(offset_gauss, 0, 6,1e-6)
+print('Adaptive Integrator:', ans, ans2, ans-(10+np.sqrt(2*np.pi)))
--- Assignment 2/test.py
+++ Assignment 2/test.py
@@ -10,7 +10,7 @@ sigma = 0.1*(4 * np.pi * R**2)**-1
 
 def to_int(theta, z):
     coeff = 2*np.pi*R**2*sigma/(4*np.pi*cons.epsilon_0)
-    return coeff*(z-R*np.sin(theta))/(R**2 + z**2 - 2*R*z*np.cos(theta))**(3/2)
+    return coeff*(z-R*np.cos(theta))*np.sin(theta)/(R**2 + z**2 - 2*R*z*np.cos(theta))**(3/2)
 
 #print(integrate.quad(to_int, -np.pi/2, np.pi/2, args=(5,)))
 
@@ -22,11 +22,11 @@ def efield(z):
 print(0.1/(4*np.pi*cons.epsilon_0*1))
 
 
-v = np.linspace(1.1, 4, 1001)
+v = np.linspace(-4, 4, 1001)
 y_data = [efield(i)[0] for i in v]
-y_coul = 0.1/(4*np.pi*cons.epsilon_0*v**2)
+#y_coul = 0.1/(4*np.pi*cons.epsilon_0*v**2)
 
 plt.clf()
 plt.plot(v, y_data, label = "")
-plt.plot(v, y_coul)
+#plt.plot(v, y_coul)
 plt.show()
