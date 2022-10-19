4d8ca5e448dc7fa43747ca66ea83c7b71c86b15c
cmdenis <christianmdenis@gmail.com>
2022-10-19 10:27:44 -0400

    ran a mcmc overnight

--- Assignment 5/a5q2.py
+++ Assignment 5/a5q2.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 from use_func import *
 import time
 import datetime
+import corner
 
 
 # Loading the data
@@ -66,14 +67,16 @@ while looper:
 
 print("\n Now MCMC part \n")
 
-newton_res = np.asarray([69, 0.022, 0.12, 0.06, 2.10e-9, 0.95])
+#newton_res = np.asarray([69, 0.022, 0.12, 0.06, 2.10e-9, 0.95])
+newton_res = np.asarray([6.89998762e+01, 2.20246320e-02, 1.20002275e-01, 5.99841862e-02, 1.58348123e-09, 9.49997838e-01])
 
-step_n = 5                                  # Number of steps to take
+step_n = 15000                                  # Number of steps to take
 
 p_init = newton_res                         # Initial Parameters
 
-step_size = np.asarray([1.51210183e-05, 2.52357154e-06, 8.80062373e-07, 1.95227730e-06, 4.06549121e-10, 5.96586242e-07]) #np.sqrt(np.diagonal(cov_mat))   # Size of steps
-
+#step_size = np.asarray([1.51210183e-05, 2.52357154e-06, 8.80062373e-07, 1.95227730e-06, 4.06549121e-10, 5.96586242e-07]) #np.sqrt(np.diagonal(cov_mat))   # Size of steps
+step_size = np.asarray([1.34880406e-04, 1.32011691e-05, 5.47334366e-06, 8.10841684e-06, 3.72439985e-09, 3.49691054e-06]) #np.sqrt(np.diagonal(cov_mat))   # Size of steps
+step_size = np.asarray([2.09578963e-03, 9.11207669e-05, 4.95658007e-05, 8.63565700e-05, 1.75261069e-08, 3.83463551e-05])
 chain = np.zeros([step_n, len(p_init)+1])   # Initializing the chain
 chain[0, 0:-1] = p_init
 
@@ -122,7 +125,7 @@ for i in tqdm(range(1, step_n)):
     chain[i, 0:-1] = cur_pos
     chain[i, -1] = cur_chi
 
-np.savetxt(file_name+".txt", chain)
+np.savetxt("runs/"+file_name+".txt", chain)
 
 # Printing values
 best_param = np.mean(chain[10:-1, :], axis = 0)
@@ -138,5 +141,8 @@ plt.legend()
 plt.xlabel("MCMC Steps")
 plt.ylabel("Parameter Value")
 plt.savefig("figs/a5q2_mcmc_parameters.jpg")
-plt.show()
+#plt.show()
 plt.clf()
+
+corner.corner(chain[:, 0:-1])
+plt.savefig("figs/a5q2_mcmc_corner.jpg")
