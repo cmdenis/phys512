3df68eabc50b9e7d8c38881368a957732cf0c668
cmdenis <christianmdenis@gmail.com>
2022-10-23 16:37:40 -0400

    finished code for q2

--- Assignment 5/a5q2.py
+++ Assignment 5/a5q2.py
@@ -1,114 +1,89 @@
+from black import out
 import numpy as np 
 from scipy import integrate
 from matplotlib import pyplot as plt
 import camb
-from tqdm import tqdm
 from use_func import *
 import time
 import datetime
 import corner
 
 
-# Loading the data
-planck = np.loadtxt('COM_PowerSpect_CMB-TT-full_R3.01.txt', skiprows=1)
-
-# Storing info from raw data
-ell = planck[:,0]
-data_l = len(ell)
-spec = planck[:,1]
-errs = 0.5 * (planck[:,2] + planck[:,3])
-
-# Guess parameters:
+# Guess parameters
 pars = np.asarray([69, 0.022, 0.12, 0.06, 2.10e-9, 0.95])
 
+# Loading the data
+planck = np.loadtxt('COM_PowerSpect_CMB-TT-full_R3.01.txt', skiprows=1) # Importing raw data
+x_data = planck[:,0]    # X axis data
+y_data = planck[:,1]    # Y axis data
+errs = 0.5 * (planck[:,2] + planck[:,3])    # Errors on data
+
+data_l = len(x_data)    # Length of data
+
+# Finding the length of synthsized data
+sim_len = len(get_spectrum(pars))
 
 
-# Starting the initial loop conditions
-looper = True
-pred = get_spectrum(pars)[:data_l]
-old_chi = chi2(pred, spec, errs)
-print("Initial Chi^2:", old_chi)
-trunc = 0
-
-
-print("\n Now MCMC part \n")
-
-#newton_res = np.asarray([69, 0.022, 0.12, 0.06, 2.10e-9, 0.95])
-newton_res = np.asarray([6.89998762e+01, 2.20246320e-02, 1.20002275e-01, 5.99841862e-02, 1.58348123e-09, 9.49997838e-01])
-
-step_n = 15000                                  # Number of steps to take
-
-p_init = newton_res                         # Initial Parameters
-
-#step_size = np.asarray([1.51210183e-05, 2.52357154e-06, 8.80062373e-07, 1.95227730e-06, 4.06549121e-10, 5.96586242e-07]) #np.sqrt(np.diagonal(cov_mat))   # Size of steps
-step_size = np.asarray([1.34880406e-04, 1.32011691e-05, 5.47334366e-06, 8.10841684e-06, 3.72439985e-09, 3.49691054e-06]) #np.sqrt(np.diagonal(cov_mat))   # Size of steps
-step_size = np.asarray([2.09578963e-03, 9.11207669e-05, 4.95658007e-05, 8.63565700e-05, 1.75261069e-08, 3.83463551e-05])
-chain = np.zeros([step_n, len(p_init)+1])   # Initializing the chain
-chain[0, 0:-1] = p_init
-
-cur_pos = chain[0, 0:-1]
-
-pred = get_spectrum(pars)[:data_l]
-cur_chi = chi2(pred, spec, errs)
-
-chain[0, -1] = cur_chi
-
-#test = step_size*np.random.randn(len(step_size))
-#print(test)
-#print(chi2(chain[0, 0:-1]+test, t, d))
-
-file_name = str(datetime.datetime.now())
-
-#assert(0==1)
-for i in tqdm(range(1, step_n)):
-
-    # Finding new position
-    new_pos = chain[i-1, 0:-1] + step_size*np.random.randn(len(step_size))
-
-    # Finding new chi square
-    pred = get_spectrum(pars)[:data_l]
-    new_chi = chi2(pred, spec, errs)
-
-    #print("The new Chi^2 is:", new_chi)
-    #print("The old Chi^2 is:", cur_chi)
-
-    if new_chi < cur_chi:
-        #print("Then we accept the change.")
-        accept = True
-    else:
-        delt = new_chi - cur_chi
-        #print("Then we don't immediately accept the change. Their difference is", delt)
+# Defining gradient taking function
+def num_derivs(fun,pars,dp):
+    A = np.empty([sim_len, len(pars)])  # Initializing the grad
+    for i in range(len(pars)):
+        pp = pars.copy()
+        pp[i] = pars[i]+dp[i]   # Parameters on right side
+        y_right = fun(pp)       # Right side of derivative
+        pp[i] = pars[i]-dp[i]   # Parameters on left side
+        y_left = fun(pp)        # Left side of derivative
         
-        prob = np.exp(-0.5*delt)
-        if np.random.rand() < prob:
-            accept = True
-        else:
-            accept = False
-    if accept:
-        cur_chi = new_chi
-        cur_pos = new_pos
+        A[:,i]=((y_right-y_left)/2/dp[i])   # Computing rows of gradient
+    return A
 
-    chain[i, 0:-1] = cur_pos
-    chain[i, -1] = cur_chi
+# Defining Newton method
+def num_newton(fun, pars, dp,x , y, sigma = 1, niter = 5):
+    
+    chi2prev = 1E12 # Initial Large Chi^2
 
-np.savetxt("runs/"+file_name+".txt", chain)
+    inv_N = np.eye(len(sigma))/sigma**2 # Inverse of error matrix
 
-# Printing values
-best_param = np.mean(chain[10:-1, :], axis = 0)
-param_std = np.std(chain[10:-1, :], axis = 0)
+    # Main Newton Method loop
+    for i in range(niter):
+    
+        pred = fun(pars)[0:data_l]              # Predicted data
+        r = y - pred                            # Residuals
+        A = num_derivs(fun, pars, dp)[0:data_l] # Gradient
 
-print("The best fit parameters are:", best_param[0:-1])
-print("Their error is:", param_std[0:-1])
+        lhs = A.T@inv_N@A                       # Finding lin alg rhs
+        rhs = A.T@inv_N@r                       # Finding lin alg lhs
+        step = np.linalg.inv(lhs)@rhs           # Finding size and direction   
+        
+        pars = pars + step                      # New position after step
+ 
+        
+        chi2 =  np.sum(r**2/sigma**2)           # Computing Chi^2
 
-# Plotting 
-plt.plot(chain[:, 0:-1], label = ["a", "t_0", "w", "b", "c", "dt"])
-plt.title("Parameters Trajectory in MCMC")
-plt.legend()
-plt.xlabel("MCMC Steps")
-plt.ylabel("Parameter Value")
-plt.savefig("figs/a5q2_mcmc_parameters.jpg")
-#plt.show()
-plt.clf()
+        # Printing stuff
+        print("Chi^2 is:", chi2)              
+        print("Chi^2 Diff is:", chi2prev - chi2)
+        print("Parameters are:", pars)
+        print('Step is:', step)
 
-corner.corner(chain[:, 0:-1])
-plt.savefig("figs/a5q2_mcmc_corner.jpg")
+        chi2prev = chi2     # Updating Chi^2
+        
+    print("Done Newton Method\n")
+    return pars, np.linalg.inv(lhs)
+
+# Chi^2 function
+def chisq(y, pred, err):
+    r=y-pred
+    return np.sum(r**2/err**2)
+
+
+# Doing Newton Method
+fun = get_spectrum
+print("Starting Newton Method...\n")
+pars, cov_mat = num_newton(fun, pars, pars*1e-8, x_data, y_data, errs, 1)
+np.savetxt("planck_fit_params.txt", pars) # Storing params on cpu
+np.savetxt("planck_fit_cov.txt", cov_mat) # Storing errors on cpu
+
+print("The best fit parameters from Newton are:", pars)
+print("Their error is:", np.sqrt(np.diagonal(cov_mat)))
+
