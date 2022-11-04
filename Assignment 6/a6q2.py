f3671ec6214d0c1b0e05a6fa061bda57e3cd8a97
cmdenis <christianmdenis@gmail.com>
2022-11-04 11:45:54 -0400

    finished q2 and started q3

--- Assignment 6/a6q2.py
+++ Assignment 6/a6q2.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 
 
 # Defining the correlation function
-def cor_func(a, b):
-    return np.fft.ifft(np.fft.fft(a) * np.conjugate(np.fft.fft(b)))
+def cor_func(a, b, shift = False):
+    if not shift:
+        return np.fft.ifft(np.fft.fft(a) * np.conjugate(np.fft.fft(b)))
+    if shift:
+        return np.fft.fftshift(np.fft.ifft(np.fft.fft(a) * np.conjugate(np.fft.fft(b))))
+
 
 
 # Part a)
@@ -13,12 +17,15 @@ def cor_func(a, b):
 x = np.linspace(-5, 5, 1001)
 y1 = np.exp(-x**2/2)/(np.sqrt(2*np.pi))
 y2 = cor_func(y1, y1)
+y4 = cor_func(y1, y1, shift = True)
 y3 = np.convolve(y1, y1, 'same')
+
 
  # plotting
 plt.plot(x, y1, label = "Gaussian")
 plt.plot(x, y2, label = "Correlation (Fourier space)")
-plt.plot(x, y3, label = "Correlation (convolution)")
+plt.plot(x, y4, label = "Correlation (Fourier space with shift)")
+#plt.plot(x, y3, label = "Correlation (convolution)")
 plt.legend()
 plt.savefig("figs/a6q2_a_gauss_cor.jpg")
 plt.show()
@@ -28,16 +35,18 @@ plt.show()
 
 # Generating our arrays
 x = np.linspace(-5, 5, 1001)
-y1 = np.exp(-(x-1)**2/2)/(np.sqrt(2*np.pi))
+y1 = np.exp(-(x-2.5)**2/2)/(np.sqrt(2*np.pi))
 y2 = cor_func(y1, y1)
-y3 = np.convolve(y1, y1, 'same')
+y4 = cor_func(y1, y1, shift = True)
+#y3 = np.convolve(y1, y1, 'same')
 
  # plotting
 plt.plot(x, y1, label = "Gaussian")
 plt.plot(x, y2, label = "Correlation (Fourier space)")
-plt.plot(x, y3, label = "Correlation (convolution)")
+plt.plot(x, y4, label = "Correlation (Fourier space with shift)")
+#plt.plot(x, y3, label = "Correlation (convolution)")
 plt.legend()
-plt.savefig("figs/a6q2_a_gauss_cor.jpg")
+plt.savefig("figs/a6q2_b_gauss_cor.jpg")
 plt.show()
 
 
