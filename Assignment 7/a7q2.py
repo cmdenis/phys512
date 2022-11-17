e1d7423df2d9c7bb3895b7a179a738a7f4ed456b
cmdenis <christianmdenis@gmail.com>
2022-11-17 11:10:40 -0500

    gaussian worknig with vectorization

--- Assignment 7/a7q2.py
+++ Assignment 7/a7q2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10, 10, 1001)
+x = np.linspace(-4, 4, 1001)
 
 
 def gaussian(x, mean, sigma):
@@ -18,26 +18,48 @@ rand_n = np.zeros(N)
 
 
 
-
+# Using a recursive function
 def gaussian_rand(mean, sigma):
     # Generating 2 random numbers
     r = np.random.rand(2)
     r[0] = r[0]*20 - 10
-
     # Check if they are accepted, else try again recursively
     if r[1] < gaussian(r[0], mean, sigma):
         return r[0]
     else:
         return gaussian_rand(mean, sigma)
 
-gaussian_data = np.array([gaussian_rand(mean, sigma) for i in range(N)])
 
 
 
-bins = np.linspace(-5, 5, 51)
+# Using vectorizing and numpy arrays
+def rand_np(n, range, pdf):
+    dist = range[1] - range[0] # Distance between highest and lowest value in desired range
+    r1 = np.random.rand(n)*dist + range[0]
+    r2 = np.random.rand(n)
+
+    accept = r2 < pdf(r1)
+
+    return r1[accept]
+
+def f(x):
+    sigma = 1
+    mean = 0
+    denominator = np.sqrt(2*np.pi)*sigma
+    return np.exp(-((x-mean)/sigma)**2/2)/denominator
+
+gaussian_data = rand_np(1000000, [x[0], x[-1]], f)
+
+print(len(gaussian_data))
+
+bins = np.linspace(x[0], x[-1], 50)
 plt.hist(gaussian_data, bins, density = True)
-plt.plot(x, gauss)
+plt.plot(x, f(x))
 plt.show()
+
+
+
+
 
 
 
